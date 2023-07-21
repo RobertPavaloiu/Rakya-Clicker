f99e9678aede00ea4f6c65faef67ddae751c726c
RobertPavaloiu <robert.pavaloiu@ziffdavis.com>
2023-07-21 15:49:32 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,62 @@
+// main button
+const rakyaButton = document.querySelector(".rakyabutton");
+let output = document.querySelector("#counter");
+clicks = document.querySelector("#clicks");
+let count = 0;
+let multiplier = 1;
+rakyaButton.addEventListener("click", function () {
+  count += multiplier;
+  output.innerHTML = `Current Count: ${count}`;
+});
+
+//upgrades
+//u1
+
+let marketButton = document.querySelector("#u1");
+function marketUpgrade() {
+  if (count >= 25) {
+    multiplier += 1;
+    count -= 25;
+    output.innerHTML = `Current Count: ${count}`;
+    clicks.innerHTML = `Rakya per click: ${multiplier}`;
+  }
+}
+marketButton.addEventListener("click", marketUpgrade);
+
+//u2
+
+let distilleryButton = document.querySelector("#u2");
+function distilleryUpgrade() {
+  if (count >= 250) {
+    multiplier += 15;
+    count -= 250;
+    output.innerHTML = `Current Count: ${count}`;
+    clicks.innerHTML = `Rakya per click: ${multiplier}`;
+  }
+}
+distilleryButton.addEventListener("click", distilleryUpgrade);
+
+//u3
+
+let volunteerButton = document.querySelector("#u3");
+function volunteerUpgrade() {
+  if (count >= 2500) {
+    multiplier += 150;
+    count -= 2500;
+    output.innerHTML = `Current Count: ${count}`;
+    clicks.innerHTML = `Rakya per click: ${multiplier}`;
+  }
+}
+volunteerButton.addEventListener("click", volunteerUpgrade);
+
+//u4
+let wideButton = document.querySelector("#u4");
+function wideUpgrade() {
+  if (count >= 25000) {
+    multiplier += 1500;
+    count -= 25000;
+    output.innerHTML = `Current Count: ${count}`;
+    clicks.innerHTML = `Rakya per click: ${multiplier}`;
+  }
+}
+wideButton.addEventListener("click", wideUpgrade);
